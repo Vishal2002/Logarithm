8bdf7e9c86455c5e3f19ef815d8a2f5e8eaeefab
Vishal <vishalsharma05052002@gmail.com>
2024-07-28 11:39:48 +0530

    Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,7 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
       <body className={inter.className}>{children}
-
+       <Analytics/>
       <Toaster/>
       </body>
      
